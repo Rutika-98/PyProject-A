d90c21b3e05cd863a346a3faad89f50fa8f1c430
Rutika <renu.rutika@gmail.com>
2023-10-22 21:13:12 +0530

    Camparision of 2 numbers

---
+++ 2_num_comparition.py
@@ -0,0 +1,10 @@
+# Create a program that takes two numbers as input and
+# prints whether the first number is greater than, less than, or equal to the second number.
+
+num1 = input("Enter the number 1 : ")
+num2 = input("Enter the number 2 : ")
+
+if num1 > num2:
+    print(num1,"is greater than ",num2)
+else:
+    print(num2,"is greater than ",num1)
