5ecb2cf05fbe50c02af621c1233851d9229b30ba
Rutika <renu.rutika@gmail.com>
2023-10-22 22:24:12 +0530

    Square and Cube

---
+++ Square&Cube.py
@@ -0,0 +1,7 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+num = int(input("Enter the number : "))
+
+print("Square of number : ",num**2)
+print("Cube of number : ",num**3)
+print("Fourth root of number : ",num**4)
